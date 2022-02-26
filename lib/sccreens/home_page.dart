852470598c35cdc26d2dbcfb40979f804daf1457
high-coder <65639323+high-coder@users.noreply.github.com>
2022-02-26 20:16:56 +0530

    home page

--- lib/sccreens/home_page.dart
+++ lib/sccreens/home_page.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:habit_heat/constants/size_config.dart';
 import 'package:habit_heat/sccreens/log_in.dart';
 import 'package:habit_heat/widgets/card.dart';
 import 'package:intl/intl.dart';
+import 'package:lottie/lottie.dart';
 import 'package:provider/provider.dart';
 import 'package:habit_heat/Provider/account.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -41,205 +43,282 @@ class _HomePageState extends State<HomePage> {
               begin: Alignment.topRight,
               end: Alignment.bottomLeft)),
       child: Scaffold(
-        drawer: Drawer(
-          child: Padding(
-            padding: const EdgeInsets.all(50.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 15),
-                  child: GestureDetector(
-                    onTap: () {
-                      Provider.of<Auth>(context, listen: false).logout(context);
-                      Navigator.of(context).popUntil((route) => route.isFirst);
-                      Navigator.pushReplacement(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => const LogIn()));
-                    },
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          'Logout',
-                          style: TextStyle(
-                              fontFamily: 'Roboto',
-                              fontSize: 20,
-                              color: Colors.red),
-                        ),
-                        Icon(
-                          Icons.logout,
-                          color: Colors.red,
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 15),
-                  child: GestureDetector(
-                    onTap: () {
-                      _launchURL();
-                    },
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          'Privacy Policy',
-                          style: TextStyle(
-                              fontFamily: 'Roboto',
-                              fontSize: 20,
-                              color: Colors.red),
-                        ),
-                        Icon(
-                          Icons.privacy_tip,
-                          color: Colors.red,
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ),
+        // drawer: Drawer(
+        //   child: Padding(
+        //     padding: const EdgeInsets.all(50.0),
+        //     child: Column(
+        //       mainAxisAlignment: MainAxisAlignment.start,
+        //       children: [
+        //         Padding(
+        //           padding: const EdgeInsets.symmetric(vertical: 15),
+        //           child: GestureDetector(
+        //             onTap: () {
+        //               Provider.of<Auth>(context, listen: false).logout(context);
+        //               Navigator.of(context).popUntil((route) => route.isFirst);
+        //               Navigator.pushReplacement(
+        //                   context,
+        //                   MaterialPageRoute(
+        //                       builder: (context) => const LogIn()));
+        //             },
+        //             child: Row(
+        //               mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        //               children: [
+        //                 Text(
+        //                   'Logout',
+        //                   style: TextStyle(
+        //                       fontFamily: 'Roboto',
+        //                       fontSize: 20,
+        //                       color: Colors.red),
+        //                 ),
+        //                 Icon(
+        //                   Icons.logout,
+        //                   color: Colors.red,
+        //                 ),
+        //               ],
+        //             ),
+        //           ),
+        //         ),
+        //         Padding(
+        //           padding: const EdgeInsets.symmetric(vertical: 15),
+        //           child: GestureDetector(
+        //             onTap: () {
+        //               _launchURL();
+        //             },
+        //             child: Row(
+        //               mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        //               children: [
+        //                 Text(
+        //                   'Privacy Policy',
+        //                   style: TextStyle(
+        //                       fontFamily: 'Roboto',
+        //                       fontSize: 20,
+        //                       color: Colors.red),
+        //                 ),
+        //                 Icon(
+        //                   Icons.privacy_tip,
+        //                   color: Colors.red,
+        //                 ),
+        //               ],
+        //             ),
+        //           ),
+        //         ),
+        //       ],
+        //     ),
+        //   ),
+        // ),
         backgroundColor: Colors.transparent,
         appBar: AppBar(
-          title: Center(
+          title:const Center(
               child: Padding(
             padding: const EdgeInsets.all(11.0),
             child: Text(
               'TODO',
               style:
-                  TextStyle(fontFamily: 'Roboto', fontWeight: FontWeight.bold),
+                  const TextStyle(fontFamily: 'Roboto', fontWeight: FontWeight.bold),
             ),
           )),
-          leading: Builder(builder: (context) {
-            return Padding(
-              padding: const EdgeInsets.all(11.0),
-              child: IconButton(
-                icon: FaIcon(FontAwesomeIcons.alignLeft),
-                onPressed: () {
-                  Scaffold.of(context).openDrawer();
+          leading: Container(
+            padding: EdgeInsets.only(left: 10),
+            width: 30,
+            height: 30,
+            // decoration: BoxDecoration(
+            //   shape: BoxShape.circle,
+            //   boxShadow: [
+            //     BoxShadow(
+            //         blurRadius: 12,
+            //         color: Colors.black38,
+            //         spreadRadius: 0)
+            //   ],
+            // ),
+
+            child: Lottie.asset("assets/lottie/profile.json"),
+            // child: CircleAvatar(
+            //     radius: 20,
+            //     child: ClipRRect(
+            //       borderRadius: BorderRadius.circular(30),
+            //       child: Image.asset(
+            //         'images/avatar.jpg',
+            //         //height: 20,
+            //         //width: 20,
+            //         //fit: BoxFit.fitWidth,
+            //       ),
+            //     )),
+          ),
+          actions: [
+            // Padding(
+            //   padding: const EdgeInsets.all(11.0),
+            //   child: IconButton(
+            //     icon: Icon(Icons.search),
+            //     onPressed: () {},
+            //   ),
+            // )
+
+            // this is the drop down menu thing in the application that will be used to
+            // select a particular item in the field
+            Padding(
+              padding: const EdgeInsets.only(right:15.0),
+              child: PopupMenuButton(
+                color: Colors.black,
+                child: Icon(
+                  Icons.menu,
+                  color: Colors.white,
+                ),
+                itemBuilder: (context) {
+                  return List.generate(1, (index) {
+                    return PopupMenuItem(
+                      onTap: () {
+
+                      },
+                      //textStyle: GoogleFonts.openSans(fontSize: 12,color:ourWhite),
+                      height: 20,
+                      child: Container(
+                          color: Colors.black,
+                          height: 20,
+
+                          child: Text("data"),
+                      ),
+                    );
+                  });
                 },
               ),
-            );
-          }),
-          actions: [
-            Padding(
-              padding: const EdgeInsets.all(11.0),
-              child: IconButton(
-                icon: Icon(Icons.search),
-                onPressed: () {},
-              ),
-            )
+            ),
+
+
           ],
           backgroundColor: Colors.transparent,
         ),
         body: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Padding(
-              padding: const EdgeInsets.only(left: 50, right: 50, top: 50),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Container(
-                    decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      boxShadow: [
-                        BoxShadow(
-                            blurRadius: 12,
-                            color: Colors.black38,
-                            spreadRadius: 0)
-                      ],
+            Expanded(
+              flex:3,
+              child: Padding(
+                padding: const EdgeInsets.only(left: 50, right: 50, top: 20),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    // Container(
+                    //   decoration: BoxDecoration(
+                    //     shape: BoxShape.circle,
+                    //     boxShadow: [
+                    //       BoxShadow(
+                    //           blurRadius: 12,
+                    //           color: Colors.black38,
+                    //           spreadRadius: 0)
+                    //     ],
+                    //   ),
+                    //   child: CircleAvatar(
+                    //       radius: 30,
+                    //       child: ClipRRect(
+                    //         borderRadius: BorderRadius.circular(30),
+                    //         child: Image.asset(
+                    //           'images/avatar.jpg',
+                    //           height: 60,
+                    //           width: 60,
+                    //           fit: BoxFit.fitWidth,
+                    //         ),
+                    //       )),
+                    // ),
+                    SizedBox(
+                      height: 5,
                     ),
-                    child: CircleAvatar(
-                        radius: 30,
-                        child: ClipRRect(
-                          borderRadius: BorderRadius.circular(30),
-                          child: Image.asset(
-                            'images/avatar.jpg',
-                            height: 60,
-                            width: 60,
-                            fit: BoxFit.fitWidth,
-                          ),
-                        )),
-                  ),
-                  SizedBox(
-                    height: 25,
-                  ),
-                  Text(
-                    'Hello,John',
-                    maxLines: 1,
-                    style: TextStyle(
-                        fontFamily: 'Roboto',
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white,
-                        fontSize: 35),
-                  ),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  Text(
-                    'Looks like feel good.\nYou have some tasks to do today.',
-                    style: TextStyle(
-                        fontFamily: 'Roboto',
-                        color: Colors.white.withOpacity(.75),
-                        fontSize: 20),
-                  ),
-                  SizedBox(
-                    height: 40,
-                  ),
-                  Text(
-                    DateFormat.yMMMMd('en_US').format(now),
-                    style: TextStyle(
-                        fontFamily: 'Roboto',
-                        color: Colors.white,
-                        fontSize: 17,
-                        fontWeight: FontWeight.bold),
-                  ),
-                ],
+                    Text(
+                      'Hello,John',
+                      maxLines: 1,
+                      style: TextStyle(
+                          fontFamily: 'Roboto',
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                          fontSize: 35,
+
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    // Text(
+                    //   'Looks like feel good.\nYou have some tasks to do today.',
+                    //   style: TextStyle(
+                    //       fontFamily: 'Roboto',
+                    //       color: Colors.white.withOpacity(.75),
+                    //       fontSize: 20),
+                    //   textAlign: TextAlign.center,
+                    //
+                    // ),
+
+                    Center(
+                      child: Row(
+                        children: [
+                          Text("Streak",style: TextStyle(
+                              fontFamily: 'Roboto',
+                              color: Colors.white,
+                              fontSize: 17,
+                              fontWeight: FontWeight.bold),),
+                          SvgPicture.asset("assets/icons/streak.svg"),
+                          Text("5",style: TextStyle(
+                              fontFamily: 'Roboto',
+                              color: Colors.white,
+                              fontSize: 17,
+                              fontWeight: FontWeight.bold),),
+                        ],
+                      ),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Text(
+                      DateFormat.yMMMMd('en_US').format(now),
+                      style: TextStyle(
+                          fontFamily: 'Roboto',
+                          color: Colors.white,
+                          fontSize: 17,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ],
+                ),
               ),
             ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 0),
-              child: SizedBox(
-                height: SizeConfig.screenHeight! * 0.5,
-                child: PageView(
-                    controller: PageController(viewportFraction: 0.75),
-                    onPageChanged: (int i) {
-                      setState(() {
-                        if (i == 1) {
-                          GColors = [Color(0xff62aee9), Color(0xff5363e2)];
-                        } else if (i == 2) {
-                          GColors = [Color(0xff4cc1a9), Color(0xff378e7c)];
-                        } else {
-                          GColors = [Color(0xfff6ac51), Color(0xfff65875)];
-                        }
-                      });
-                    },
-                    //shrinkWrap: true,
-                    //  physics: PageScrollPhysics(),
-                    scrollDirection: Axis.horizontal,
-                    children: [
-                      TaskCard(
-                        profile: 'Personal',
-                        icon: Icons.person,
-                        color: [Color(0xfff6ac51), Color(0xfff65875)],
-                      ),
-                      TaskCard(
-                        profile: 'Work',
-                        icon: Icons.work,
-                        color: [Color(0xff62aee9), Color(0xff5363e2)],
-                      ),
-                      TaskCard(
-                        profile: 'Home',
-                        icon: Icons.home,
-                        color: [Color(0xff4cc1a9), Color(0xff378e7c)],
-                      )
-                    ]),
+            Expanded(
+              flex: 7,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 0),
+                child: SizedBox(
+                  height: SizeConfig.screenHeight! * 0.5,
+                  child: PageView(
+                      controller: PageController(viewportFraction: 0.75),
+                      onPageChanged: (int i) {
+                        setState(() {
+                          if (i == 1) {
+                            GColors = [Color(0xff62aee9), Color(0xff5363e2)];
+                          } else if (i == 2) {
+                            GColors = [Color(0xff4cc1a9), Color(0xff378e7c)];
+                          } else {
+                            GColors = [Color(0xfff6ac51), Color(0xfff65875)];
+                          }
+                        });
+                      },
+                      //shrinkWrap: true,
+                      //  physics: PageScrollPhysics(),
+                      scrollDirection: Axis.horizontal,
+                      children: [
+                        TaskCard(
+                          profile: 'Personal',
+                          icon: Icons.person,
+                          color: [Color(0xfff6ac51), Color(0xfff65875)],
+                        ),
+                        TaskCard(
+                          profile: 'Work',
+                          icon: Icons.work,
+                          color: [Color(0xff62aee9), Color(0xff5363e2)],
+                        ),
+                        TaskCard(
+                          profile: 'Home',
+                          icon: Icons.home,
+                          color: [Color(0xff4cc1a9), Color(0xff378e7c)],
+                        )
+                      ]),
+                ),
               ),
             )
           ],
